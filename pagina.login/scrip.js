429985a7fbe211626877b7aad7eaf841ade6f069
juanvalencia1710-netizen <juanvalencia1710@gmail.com>
2025-12-18 18:15:54 -0500

    Add files via upload

---
+++ pagina.login/scrip.js
@@ -0,0 +1,10 @@
+const carrito = document.querySelector(".carrito");
+
+carrito.addEventListener("click", () => {
+    carrito.classList.toggle("abierto");
+});
+
+
+li.textContent = `${nombre} - $${precio.toLocaleString()} COP`;
+
+totalSpan.textContent = total.toLocaleString();
